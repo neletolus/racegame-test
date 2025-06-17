3892506d01d31fc3377b8d8776f52892f473cf04
Yohei Suda <y1405705@gmail.com>
2025-06-18 08:14:46 +0900

    Add GameController script and scene settings for player and environment

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+  public Rigidbody theRB;
+  public float maxSpeed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+  void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
